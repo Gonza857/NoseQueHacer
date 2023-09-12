1446d19ac66d1f320753b8afcf5ac298027602d1
Gonzalo Ramos <gonzaloalexramos@gmail.com>
2023-09-12 16:53:04 -0300

    saassa

--- Prueba22/src/prueb/Yacasi.java
+++ Prueba22/src/prueb/Yacasi.java
@@ -4,11 +4,10 @@ public class Yacasi {
 
 	//oaoaoaoaoaaaa
 	
-<<<<<<< HEAD
+
 	// tamo bien, sobran los billetes de 100
-=======
+
 	//commit1 suerte my friends
 	
 	//commit2 mas suerte todavia my friend
->>>>>>> branch 'master' of https://github.com/Gonza857/NoseQueHacer
 }
